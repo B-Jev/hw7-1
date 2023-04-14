cfda35f94200bde9f6fbb11505f8242596f561ee
B-Jev <baldenmax@gmail.com>
2023-04-14 21:45:12 +0300

    Initial commit

---
+++ src/test/java/com/example/hw71/Hw71ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw71;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw71ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
